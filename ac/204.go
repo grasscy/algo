f4dd2dfc4406fda81ee6f4b270ac48aacaa1fd8c
grasscy <wangjlc@qq.com>
2022-10-19 14:53:48 +0800

    day57

---
+++ ac/204.go
@@ -0,0 +1,22 @@
+package ac
+
+func countPrimes(n int) int {
+	isprime := make([]bool, n)
+	for i := 0; i < n; i++ {
+		isprime[i] = true
+	}
+	for i := 2; i*i < n; i++ {
+		if isprime[i] {
+			for j := i * i; j < n; j += i {
+				isprime[j] = false
+			}
+		}
+	}
+	var ans int
+	for i := 2; i < n; i++ {
+		if isprime[i] {
+			ans++
+		}
+	}
+	return ans
+}
