ddf5ad3430fa8f2588e53f9d9f0d2d8bdb1dda4b
grasscy <wangjlc@qq.com>
2022-10-20 13:25:48 +0800

    day58

---
+++ 397.go
@@ -0,0 +1,18 @@
+package algo
+
+func integerReplacement(n int) int {
+	ans := 0
+	return d(n, ans)
+}
+
+func d(n int, ans int) int {
+	if n == 1 {
+		return ans
+	}
+	if n%2 == 0 {
+		return d(n/2, ans+1)
+	} else {
+		return min(d(n-1, ans+1), d(n+1, ans+1))
+	}
+	return ans
+}
