8ee75879146cb59f8feea458648a056f78f63429
grasscy <wangjlc@qq.com>
2022-11-06 17:45:49 +0800

    day73

---
+++ 646.go
@@ -0,0 +1,23 @@
+package algo
+
+import "sort"
+
+func findLongestChain(pairs [][]int) int {
+	sort.Slice(pairs, func(i, j int) bool {
+		if pairs[i][0] == pairs[j][0] {
+			return pairs[i][1] < pairs[j][1]
+		}
+		return pairs[i][0] < pairs[j][0]
+	})
+	dp := make([]int, len(pairs))
+	dp[0] = 1
+	for i := 1; i < len(pairs); i++ {
+		dp[i] = 1
+		for j := i - 1; j >= 0; j-- {
+			if pairs[i][0] > pairs[j][1] && dp[j]+1 > dp[i] {
+				dp[i] = dp[j] + 1
+			}
+		}
+	}
+	return dp[len(dp)-1]
+}
