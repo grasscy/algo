7efc5c2cc9e3cc510f0a5f2e2256f68f216ec9fe
grasscy <wangjlc@qq.com>
2022-11-12 14:39:18 +0800

    day79

--- 10.go
+++ 10.go
@@ -7,21 +7,19 @@ func isMatch(s string, p string) bool {
 	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]bool, len(p)+1)
 	}
-	if s[0] == p[0] || p[0] == '.' {
-		dp[1][1] = true
-	}
+	dp[0][0] = true
 
 	for i := 1; i <= len(s); i++ {
 		for j := 1; j <= len(p); j++ {
 			if p[j-1] == '.' {
 				dp[i][j] = dp[i-1][j-1]
 			} else if p[j-1] >= 'a' && p[j-1] <= 'z' {
-				dp[i][j] = dp[i-1][j-1] && s[i] == p[j]
+				dp[i][j] = dp[i-1][j-1] && s[i-1] == p[j-1]
 			} else {
 				if p[j-2] == '.' {
 					dp[i][j] = dp[i-1][j-1]
 				} else {
-					dp[i][j] = dp[i-1][j-1] && s[i] == p[j-1]
+					dp[i][j] = dp[i-1][j-1] && s[i-1] == p[j-2]
 				}
 			}
 		}
